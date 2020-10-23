f3b9e5c7b7f0a969b3e1b71f3f0352137beba385
Fina Silva-Santisteban <fina@ltse.com>
2020-10-23 09:48:03 -0700

    add coding kata and setup

---
+++ code_challenges/wolf_in_sheeps_clothing.py
@@ -0,0 +1,5 @@
+# https://www.codewars.com/kata/5c8bfa44b9d1192e1ebd3d15
+
+
+def warn_the_sheep(animals_queue):
+    pass
---
+++ test/test_wolf_in_sheeps_clothing.py
@@ -0,0 +1,12 @@
+import unittest
+from code_challenges.wolf_in_sheeps_clothing import warn_the_sheep
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_something(self):
+        self.assertEqual(False, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
