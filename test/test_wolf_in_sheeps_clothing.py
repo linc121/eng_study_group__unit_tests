fe4f5f26ea9f593a4be2ea6b2ff46714db450e6e
Fina Silva-Santisteban <fina@ltse.com>
2020-10-23 13:18:56 -0700

    add changes

--- test/test_wolf_in_sheeps_clothing.py
+++ test/test_wolf_in_sheeps_clothing.py
@@ -2,10 +2,18 @@ import unittest
 from code_challenges.wolf_in_sheeps_clothing import warn_the_sheep
 
 
-class MyTestCase(unittest.TestCase):
+class MyTestWolfCase(unittest.TestCase):
 
-    def test_something(self):
-        self.assertEqual(False, False)
+    def test_method_exists(self):
+        self.assertTrue(warn_the_sheep())
+
+    def test_returns_pls_go_away_sentence(self):
+        sentence_to_test = 'Pls go away and stop eating my sheep'
+        self.assertEqual(sentence_to_test, warn_the_sheep())
+
+    def test_returns_warning_sentence(self):
+        sentence_to_test = 'Oi! Sheep number N! You are about to be eaten by a wolf!'
+        self.assertEqual(sentence_to_test, warn_the_sheep('something'))
 
 
 if __name__ == '__main__':
