f58899dcd9d338abad4a11f9e66c80448d7d97a6
lincoln li <lincoln.kun.li@gmail.com>
2020-10-30 10:47:39 -0400

    added lincoln folder

---
+++ lincoln/code_challenges/wolf_in_sheeps_clothing/wolf_in_sheeps_clothing.py
@@ -0,0 +1,8 @@
+# https://www.codewars.com/kata/5c8bfa44b9d1192e1ebd3d15
+# (See screenshot in same folder.)
+
+
+def warn_the_sheep(sheep_wolf_queue=None):
+    if sheep_wolf_queue:
+        return 'Oi! Sheep number N! You are about to be eaten by a wolf!'
+    return 'Pls go away and stop eating my sheep'
