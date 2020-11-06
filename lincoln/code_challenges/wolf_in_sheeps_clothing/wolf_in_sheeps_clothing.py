18347b583318018bf4908e916f471a541dd70cb1
lincoln li <lincoln.kun.li@gmail.com>
2020-11-06 15:27:53 -0500

    Lincoln's Update

--- lincoln/code_challenges/wolf_in_sheeps_clothing/wolf_in_sheeps_clothing.py
+++ lincoln/code_challenges/wolf_in_sheeps_clothing/wolf_in_sheeps_clothing.py
@@ -3,6 +3,12 @@
 
 
 def warn_the_sheep(sheep_wolf_queue=None):
-    if sheep_wolf_queue:
-        return 'Oi! Sheep number N! You are about to be eaten by a wolf!'
-    return 'Pls go away and stop eating my sheep'
+    if type(sheep_wolf_queue) is list:
+        if sheep_wolf_queue[-1] == 'wolf':
+            return 'Pls go away and stop eating my sheep'
+        else:
+            index = sheep_wolf_queue.index('wolf')
+            length = len(sheep_wolf_queue)
+            return f'Oi! Sheep number {length - index-1}! You are about to be eaten by a wolf!'
+    else:
+        return "sheep_wolf_queue is not a list"
--- lincoln/test/test_wolf_in_sheeps_clothing.py
+++ lincoln/test/test_wolf_in_sheeps_clothing.py
@@ -1,5 +1,5 @@
 import unittest
-from fina.code_challenges.wolf_in_sheeps_clothing.wolf_in_sheeps_clothing import warn_the_sheep
+from code_challenges.wolf_in_sheeps_clothing.wolf_in_sheeps_clothing import warn_the_sheep
 
 
 class MyTestWolfCase(unittest.TestCase):
@@ -10,15 +10,22 @@ class MyTestWolfCase(unittest.TestCase):
     #TODO: We forgot to test the parameter! Let's check if warn_the_sheep() takes ina parameter
     # And another test for checking if parameter is list.
 
+    def test_non_list_parameter(self):
+        sentence_to_test = "sheep_wolf_queue is not a list"
+        self.assertEqual(sentence_to_test, warn_the_sheep())
+
+
     def test_returns_pls_go_away_sentence(self):
         sentence_to_test = 'Pls go away and stop eating my sheep'
         #TODO: There's another requirement for this test: if the wolf is the closest animal to you, return ^
-        self.assertEqual(sentence_to_test, warn_the_sheep())
+        self.assertEqual(sentence_to_test, warn_the_sheep(['sheep', 'sheep', 'wolf']))
 
     def test_returns_warning_sentence(self):
-        sentence_to_test = 'Oi! Sheep number N! You are about to be eaten by a wolf!'
+        sentence1_to_test = 'Oi! Sheep number 1! You are about to be eaten by a wolf!'
+        sentence2_to_test = 'Oi! Sheep number 2! You are about to be eaten by a wolf!'
         #TODO: There's another requirement for this test: if the wolf is NOT the closest animal to you, return ^
-        self.assertEqual(sentence_to_test, warn_the_sheep('something'))
+        self.assertEqual(sentence1_to_test, warn_the_sheep(['sheep', 'sheep', 'wolf', 'sheep']))
+        self.assertEqual(sentence2_to_test, warn_the_sheep(['sheep', 'sheep', 'wolf', 'sheep', 'sheep']))
 
 
 if __name__ == '__main__':
